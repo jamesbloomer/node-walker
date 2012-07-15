928c6aae5d12a5b77427765c7eddd8a2f6ccbbbf
James Bloomer <github2@thebloomers.co.uk>
2012-07-15 19:51:03 +0100

    correct mistake in error handling

--- example/walk.js
+++ example/walk.js
@@ -1,8 +1,12 @@
 var walker = require('../lib/walker.js');
 
 walker.walk(process.argv[2], process.argv[3], function(location, done) {
-  console.log('do stuff %s', JSON.stringify(location));
-  done();
-},function(e) {
-  process.exit(0);
+    console.log('do stuff %s', JSON.stringify(location));
+    done();
+}, function(e) {
+    if(e) {
+        console.log(e);
+    }
+
+    process.exit(0);
 });
